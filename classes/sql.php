fa9d8a0b2b44942582906b885d54978e17c6da01
betepah42 <sharapov42@yandex.ru>
2022-10-25 22:14:15 +0300

    Prava

--- classes/sql.php
+++ classes/sql.php
@@ -36,6 +36,7 @@ class SQL
 
     public static function q($sql, $params = [])
     {
+        file_put_contents("sql.txt", "Q:". $sql,FILE_APPEND);
         $dbConnection = self::init();
 
         try {
@@ -64,6 +65,7 @@ class SQL
 
     public static function q1($sql, $params = [])
     {
+        file_put_contents("sql.txt",  "Q1:".$sql,FILE_APPEND);
         $dbConnection = self::init();
 
         try {
